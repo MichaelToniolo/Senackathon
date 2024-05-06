2df7bd8c00b599a1b7a97551b93f6c4ec2640d6f
Michael (a.k.a) Mz3ro <michaelsaraivatoniolo@gmail.com>
2024-05-06 12:23:26 -0300

    Upload de Menu
    Só os Deuses sabem como ajustar o CSS.
    
    Mas vou saber em breve!! Amém

--- area.php
+++ area.php
@@ -7,14 +7,28 @@
     <title>CONHEÇA O PARTICIPANTE</title>
     <link rel="stylesheet" href="estilo.css">
     <link href="https://fonts.cdnfonts.com/css/technology" rel="stylesheet">
+    <link rel="stylesheet" href="menu.css">
     <link href="https://fonts.cdnfonts.com/css/zuma" rel="stylesheet">
+    <!-- ICONE MENU -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 </head>
 
 <body>
     <div class="global">
+        <div class="topnav">
         <div class="titulo"><b>1º SENACKATHON</b><text>_</text></div>
-        
+
+            <div id="myLinks">
+                <a href="#news">News</a>
+                <a href="#contact">Contact</a>
+                <a href="#about">About</a>
+            </div>
+            <a href="javascript:void(0);" class="icon" onclick="myFunction()" >
+                <i class="fa fa-bars"></i>
+            </a>
+        </div>
+
         <div class="titulo"><b>Partner's Skills</b><text>_</text></div>
         <div class="bio-container">
             <div class="bio1">
@@ -50,9 +64,18 @@
                 <label>SKILL 1</label>
                 <text name='skill'>TEXTO PARA COLOCAR TODAS AS SKILLS DO PARTICIPANTE.</text>
             </div>
-    </div>
+        </div>
 
-
+        <script>
+            function myFunction() {
+                var x = document.getElementById("myLinks");
+                if (x.style.display === "block") {
+                    x.style.display = "none";
+                } else {
+                    x.style.display = "block";
+                }
+            }
+        </script>
 
 </body>
 
