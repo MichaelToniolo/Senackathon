99444f8490f8f792201b22eba14a690e43d0d3db
Michael (a.k.a) Mz3ro <michaelsaraivatoniolo@gmail.com>
2024-05-06 11:50:47 -0300

    Upload de Layout de Portal do Participante

---
+++ area.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CONHEÇA O PARTICIPANTE</title>
+    <link rel="stylesheet" href="estilo.css">
+    <link href="https://fonts.cdnfonts.com/css/technology" rel="stylesheet">
+    <link href="https://fonts.cdnfonts.com/css/zuma" rel="stylesheet">
+
+</head>
+
+<body>
+    <div class="global">
+        <div class="titulo"><b>1º SENACKATHON</b><text>_</text></div>
+        
+        <div class="titulo"><b>Partner's Skills</b><text>_</text></div>
+        <div class="bio-container">
+            <div class="bio1">
+                <img src="icons/icon_skill.png">
+                <label>SKILL 1</label>
+                <text name='skill'>TEXTO PARA COLOCAR TODAS AS SKILLS DO PARTICIPANTE. PODENDO SER QUALQUER SKILL</text>
+            </div>
+
+
+            <!-- APENAS PARA TEST DE QUANTIDADE -->
+            <!-- DEPOIS LANÇAR UM WHILE TRAZENDO UM CARD COLORIDO PARA CADA SKILL E UM ICONE TAMBÉM -->
+            <div class="bio2">
+                <img src="icons/icon_skill.png">
+                <label>SKILL 1</label>
+                <text name='skill'>TEXTO PARA COLOCAR TODAS AS SKILLS DO PARTICIPANTE. PODENDO SER QUALQUER SKILL</text>
+            </div>
+            <div class="bio3">
+                <img src="icons/icon_skill.png">
+                <label>SKILL 1</label>
+                <text name='skill'>TEXTO PARA COLOCAR TODAS AS SKILLS DO PARTICIPANTE. PODENDO SER QUALQUER SKILL</text>
+            </div>
+            <div class="bio4">
+                <img src="icons/icon_skill.png">
+                <label>SKILL 1</label>
+                <text name='skill'>TEXTO PARA COLOCAR TODAS AS SKILLS DO PARTICIPANTE. PODENDO SER QUALQUER SKILL</text>
+            </div>
+            <div class="bio5">
+                <img src="icons/icon_skill.png">
+                <label>SKILL 1</label>
+                <text name='skill'>TEXTO PARA COLOCAR TODAS AS SKILLS DO PARTICIPANTE. PODENDO SER QUALQUER SKILL</text>
+            </div>
+            <div class="bio6">
+                <img src="icons/icon_skill.png">
+                <label>SKILL 1</label>
+                <text name='skill'>TEXTO PARA COLOCAR TODAS AS SKILLS DO PARTICIPANTE. PODENDO SER QUALQUER SKILL</text>
+            </div>
+    </div>
+
+
+
+</body>
+
+</html>
